4f8f11d5e0e62ed75975687a82b87fae4ae9390d
ezekiel-spec <your-email@example.com>
2026-04-14 16:05:26 +0100

    swagger updated

--- swagger.js
+++ swagger.js
@@ -5,11 +5,14 @@ const doc = {
     title: 'Team Project API',
     description: 'Media Library API for Movies and Actors'
   },
-  host: 'cse341-team-project.onrender.com', // Replace with your actual Render URL
-  schemes: ['https']
+  // Setting host to null or an empty string allows Swagger to 
+  // automatically use the domain it is currently being hosted on.
+  host: 'cse341-team-project-2.onrender.com', 
+  schemes: ['https', 'http']
 };
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./routes/index.js'];
 
+// Generate the output file
 swaggerAutogen(outputFile, endpointsFiles, doc);
